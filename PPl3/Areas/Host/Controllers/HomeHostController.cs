28fe9b114fd4c261595862488ddd1522306c06a0
Thao <hosithao1622004@gmail.com>
2024-04-23 22:46:42 +0700

    fishing list_listing

--- PPl3/Areas/Host/Controllers/HomeHostController.cs
+++ PPl3/Areas/Host/Controllers/HomeHostController.cs
@@ -27,5 +27,36 @@ namespace PPl3.Areas.Host.Controllers
             else return RedirectToAction("Login", "homeuser", new { area = "User" });
 
         }
+
+        [HttpPost]
+        public JsonResult DeleteListing(int id)
+        {
+            user p_user = (user)Session["user"];
+            PPL3Entities db = new PPL3Entities();
+            property find_hotel = db.properties.FirstOrDefault(item => item.id == id);
+            db.properties.Remove(find_hotel);
+            db.SaveChanges();
+            return Json("true" , JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult SearchListing(string text)
+        {
+            user p_user = (user)Session["user"];
+            PPL3Entities db = new PPL3Entities();
+            // Select only the required properties instead of the full entity
+            var data = (from item in db.properties
+                        where item.userId == p_user.id && item.p_name.Contains(text)
+                        select new { item.id, item.p_name, item.p_address, item.created,item.room_type.rt_name,item.property_images.FirstOrDefault().pi_image }).ToList();
+            return Json(new
+            {
+                list_pro = data,
+                success = true,
+                count = data.Count
+            }, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
+
+
