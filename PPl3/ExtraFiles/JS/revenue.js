47af2d8a087887341e79c62321b41e9ac6e42eac
Toankhoaaa <toanledinh76@gmail.com>
2024-04-28 10:01:55 +0700

    upload page revenue

--- PPl3/ExtraFiles/JS/revenue.js
+++ PPl3/ExtraFiles/JS/revenue.js
@@ -1,67 +1,143 @@
-const peopleNumber = document.querySelector('.people')
-const reviewNumber = document.querySelector('.review')
-const earningNumber = document.querySelector('.earning')
-// Số đã chọn
-const targetNumber = parseFloat(peopleNumber.textContent, 10);
-const targetNumber1 = Number.parseInt(reviewNumber.textContent, 10);
-const targetNumber2 = parseFloat(earningNumber.textContent);
-console.log(targetNumber2)
-// Thời gian delay giữa các bước
-const delay = 8; // milliseconds
-// Số bước để đạt được số đã chọn
-const steps = 150;
-const steps1 = 150;
-const steps2 = 150;
-// Bước tăng giá trị sau mỗi lần cập nhật
-const stepValue = targetNumber / steps;
-const stepValue1 = targetNumber1 / steps1;
-const stepValue2 = targetNumber2 / steps2;
-console.log(stepValue2)
-// Lấy phần tử div hiển thị số
-// Biến đếm hiện tại
-var currentNumber = 0;
-var currentNumber1 = 0;
-var currentNumber2 = 0;
-// Hàm cập nhật số
-function updateCount() {
-    // Tăng giá trị hiện tại
-    currentNumber += stepValue;
-    // Đảm bảo không vượt quá giá trị đã chọn
-    currentNumber = Math.min(currentNumber, targetNumber);
-    // Hiển thị giá trị hiện tại
-    peopleNumber.textContent = currentNumber.toFixed(3);
-    // Kiểm tra nếu đã đạt được số đã chọn
-    if (currentNumber >= targetNumber) {
-        clearInterval(interval);
+//Xử lý My Hotel
+
+const statuses = document.querySelectorAll('.box_item span')
+const hotels = document.querySelectorAll('.box_item')
+statuses.forEach((st) =>{
+    if(st.textContent === 'In Progress'){
+        st.style.backgroundColor = 'chartreuse'
     }
+    else if (st.textContent === 'Return'){
+        st.style.backgroundColor = 'red'
+    }
+    else if (st.textContent === 'Pending'){
+        st.style.backgroundColor = 'rgb(228, 109, 25)'
+    }
+})
+
+hotels.forEach((hotel) =>{
+    hotel.addEventListener('click', () =>{
+        document.querySelector('.focus').classList.remove('focus')
+        hotel.classList.add('focus')
+    })
+})
+
+//Xử lý mess, notifi
+
+const btnMess = document.querySelector('.btn_mess')
+const btnNotifi = document.querySelector('.btn_notifi')
+const header = document.getElementById('header')
+const content = document.querySelector('.main')
+const modalMess = document.querySelector('.mess')
+const  modalNotifi = document.querySelector('.notifi')
+
+function showModal(modal){
+    modal.classList.add('open')
 }
 
-function updateCount1() {
-    // Tăng giá trị hiện tại
-    currentNumber1 += stepValue1;
-    // Đảm bảo không vượt quá giá trị đã chọn
-    currentNumber1 = Math.min(currentNumber1, targetNumber1);
-    // Hiển thị giá trị hiện tại
-    reviewNumber.textContent = Math.round(currentNumber1);
-    // Kiểm tra nếu đã đạt được số đã chọn
-    if (currentNumber1 >= targetNumber1) {
-        clearInterval(interval1);
-    }
+function hideModal(modal){
+    modal.classList.remove('open')
 }
 
-function updateCount2() {
-    // Tăng giá trị hiện tại
-    currentNumber2 += stepValue2;
-    // Đảm bảo không vượt quá giá trị đã chọn
-    currentNumber2 = Math.min(currentNumber2.toFixed(3), targetNumber2);
-    // Hiển thị giá trị hiện tại
-    earningNumber.textContent = currentNumber2.toFixed(3)
-    // Kiểm tra nếu đã đạt được số đã chọn
-    if (currentNumber2 >= targetNumber2) {
-        clearInterval(interval2);
+btnMess.addEventListener('click', (e) =>{
+    if(modalMess.classList.contains('open')){
+        hideModal(modalMess)
+    }else{
+        showModal(modalMess)
+        hideModal(modalNotifi)
     }
-}
-// Bắt đầu cập nhật số sau mỗi khoảng thời gian
-const interval = setInterval(updateCount, delay);
-const interval1 = setInterval(updateCount1, delay);
-const interval2 = setInterval(updateCount2, delay);
+    e.stopPropagation();
+})
+
+btnNotifi.addEventListener('click', (e)=>{
+    if(modalNotifi.classList.contains('open')){
+        hideModal(modalNotifi)
+    }else{
+        showModal(modalNotifi)
+        hideModal(modalMess)
+    }
+    e.stopPropagation();
+})
+
+modalMess.addEventListener('click', (e)=>{
+    e.stopPropagation();
+})
+
+modalNotifi.addEventListener('click', (e)=>{
+    e.stopPropagation();
+})
+
+header.addEventListener('click', ()=>{
+    if(modalMess.classList.contains('open')){
+        hideModal(modalMess)
+    }
+    else if(modalNotifi.classList.contains('open')){
+        hideModal(modalNotifi)
+    }
+})
+
+content.addEventListener('click', () =>{
+    if(modalMess.classList.contains('open')){
+        hideModal(modalMess)
+    }
+    else if(modalNotifi.classList.contains('open')){
+        hideModal(modalNotifi)
+    }
+})
+
+
+//Show all comment 
+let commentDetail = document.getElementById('comment_detail')
+let commentDetailContainer = document.querySelector('.comment_detail_container')
+let showComment = document.querySelector('.show_all_comment')
+let commentBox = document.querySelector('.comment_box')
+showComment.addEventListener('click', () =>{
+    commentDetail.classList.add('active')
+})
+
+commentDetail.addEventListener('click', () =>{
+    commentDetail.classList.remove('active')
+})
+
+commentBox.addEventListener('click', (e) =>{
+    e.stopPropagation();
+})
+
+commentDetailContainer.addEventListener('click', (e) =>{
+    e.stopPropagation();
+})
+
+//Show all list room
+let listRoom = document.getElementById('list_room')
+let btnListRoom = document.querySelector('.show_all_list_room')
+let listRoomContainer = document.querySelector('.list_room_container')
+
+btnListRoom.addEventListener('click', ()=>{
+    listRoom.classList.add('active')
+})
+
+listRoom.addEventListener('click', () =>{
+    listRoom.classList.remove('active')
+})
+
+listRoomContainer.addEventListener('click', (e)=>{
+    e.stopPropagation();
+})
+
+
+//Show all list current of tenants
+
+let listCurrent = document.getElementById('list_current')
+let btnListCurrent = document.querySelector('.show_all_list_current')
+let listCurrentBox = document.querySelector('.current_box')
+
+btnListCurrent.addEventListener('click', ()=>{
+    listCurrent.classList.add('active')
+})
+
+listCurrent.addEventListener('click', () =>{
+    listCurrent.classList.remove('active')
+})
+
+listCurrentBox.addEventListener('click', (e)=>{
+    e.stopPropagation();
+})
