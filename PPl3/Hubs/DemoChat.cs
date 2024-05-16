a1f98798970b9f30e5ad76c580cceda4c8336699
Thao <hosithao1622004@gmail.com>
2024-05-16 08:02:27 +0700

    litte change

--- PPl3/Hubs/DemoChat.cs
+++ PPl3/Hubs/DemoChat.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+using PPl3.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace PPl3.Hubs
@@ -10,17 +12,37 @@ namespace PPl3.Hubs
     [HubName("chat")]
     public class DemoChat : Hub
     {
-        public void Hello()
-        {
-            Clients.All.hello();
-        }
 
-        public void SendPrivateMessage(int sentUserId ,int toUserId, string message)
-
+        public override Task OnConnected()
         {
 
-            Clients.All.receiveMessage(sentUserId , toUserId , message);
+            var user = (user)HttpContext.Current.Session["user"];
+
+            Groups.Add(Context.ConnectionId, user.id.ToString()); // Sử dụng ID của user làm group name
+
+            return base.OnConnected();
 
         }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+
+            var user = (user)HttpContext.Current.Session["user"];
+
+            Groups.Remove(Context.ConnectionId, user.id.ToString()); // Xóa user ra khỏi group khi ngắt kết nối
+
+            return base.OnDisconnected(stopCalled);
+
+        }
+
+        public void SendMessage(string receiverUserId, string message)
+        {
+
+            var sender = (user)HttpContext.Current.Session["user"];
+
+            Clients.Group(receiverUserId).receiveMessage(sender.id.ToString(), message); // Gửi tin nhắn đến group của receiver
+
+        }
+
     }
 }
