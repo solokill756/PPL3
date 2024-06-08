8756c23a6c6dc3860da9d60749f8aabef80a7203
Thao <hosithao1622004@gmail.com>
2024-06-08 23:13:27 +0700

    fix invoice page

--- PPl3/Models/property_reviews.cs
+++ PPl3/Models/property_reviews.cs
@@ -27,7 +27,6 @@ namespace PPl3.Models
         public string comment { get; set; }
         public Nullable<decimal> overall_rating { get; set; }
         public Nullable<System.DateTime> created { get; set; }
-        public Nullable<System.DateTime> modified { get; set; }
         public Nullable<byte> pr_status { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
